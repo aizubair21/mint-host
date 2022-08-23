717b4d98e0b442a4a6e905801726799abcb69c07
Amimul Ihsan Zubair <86657812+aizubair21@users.noreply.github.com>
2022-08-23 15:47:22 +0600

    document setup

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MinHost</title>
+
+    <!-- bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" />
+    <!-- custorm css -->
+    <link rel="stylesheet" href="mint_style.css">
+
+</head>
+
+<body>
+    <section id="mint_home_banner">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center banner_div">
+                <div class="col-lg-7 ">
+                    <div class="mint_home_banner_content">
+                        <div class="mint_home_banner_content_desc">
+                            <h1 class="h1 mint-section-title">
+                                MintHost Web hosting plan
+                            </h1>
+                            <p class="h5 mint_section-desc">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, ut.
+                            </p>
+                        </div>
+
+                        <div class="mint_home_banner_domain_search">
+                            <form action="#" method="post" class="d-flex align-items-center">
+                                <input type="search" name="search" id="d-search" placeholder="search your domain....">
+                                <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-lg-5">
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+</body>
+
+
+</html>
---
+++ partial/mint_footer.php
@@ -0,0 +1,14 @@
+<footer id="mint_home_footer">
+
+</footer>
+
+</body>
+<!-- bootstrap -->
+<script src="/assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!-- better nav  -->
+<script src="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.js"></script>
+
+
+<!-- custom css -->
+<script src="/assets/js/mint_script.js"></script>
